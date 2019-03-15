4dbfd95f03817934e686ea11158ec55deb734667
kazeem <kazeem0825@gmail.com>
2019-03-16 00:55:50 +0100

    put method added

--- api/api.js
+++ api/api.js
@@ -3,10 +3,12 @@ const app = express();
 const users = require("../users/users");
 app.use(express.json());
 
+//Getting the users
 app.get("/api/users", (req, res) => {
   res.send(users);
 });
 
+//Creating a new user
 app.post("/api/users", (req, res) => {
   const user = {
     id: users.length + 1,
@@ -14,6 +16,13 @@ app.post("/api/users", (req, res) => {
   };
 
   users.push(user);
+  res.send(user);
+});
+
+//Updating an existing user
+app.put("/api/courses/:id", (req, res) => {
+  const user = users.find(eachUser.id === parseInt(req.param.id));
+  user.name === req.body.name;
   res.send(user);
 });
 
